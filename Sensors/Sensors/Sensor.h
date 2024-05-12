47d96073b6c43c8eb85d36cb1832f5c7c1e78da9
Piotr Kozłowski <01186976@pw.edu.pl>
2024-05-12 16:03:17 +0200

    Implemented base class Sensor.

---
+++ Sensors/Sensors/Sensor.h
@@ -0,0 +1,48 @@
+#pragma once
+class Sensor {
+///		Bazowa klasa Sensor ma stale pobieraæ po³o¿enie (x, y) oraz zwrot robota (z osobnej klasy "modu³ GPS"???).
+/// 	Na podstawie po³o¿enia i zwrotu:
+///			1) ForwardSensor 
+///				- bedzie wiedzia³, czy w jego zasiêgu znajduje sie przeszkoda,
+///				- bedzie oblicza³ odleg³oœæ do w.w. przeszkody,
+///				- w razie minimalnej odleg³oœci wyœle do robota sygna³ STOP,
+///			2) SideSensor
+///				- bêdzie wiedzia³, czy w jego zasiêgu jest przeszkoda (tu¿ obok robota),
+///				- bêdzie zwraca³ informacjê typu bool ("Czy po bokach jest pusto?"),
+///				- mo¿na dodaæ kolejne dziedziczenie (Right Side Sensor i Left Side Sensor)
+/// 	Za pomoc¹ tych czujników Benek bêdzie móg³ stosowaæ manewry omijania i zawracania.
+private:
+	int x_coord, y_coord, range;
+	Direction heading;
+public:
+	//Constructor
+	Sensor(int x, int y, Direction h, int r);
+
+	//Virtual deconstructor
+	virtual ~Sensor() {};
+
+	//Getters
+	int get_x() const;
+	int get_y() const;
+	int get_range() const;
+	Direction get_heading() const;
+
+	//Setters
+	void set_x(int);
+	void set_y(int);
+	void set_range(int);
+	void set_heading(Direction);
+	
+	bool update_position(int, int, Direction);
+
+	virtual int get_dist_to_obstacle() const = 0;
+
+	virtual bool obstacle_in_range() const = 0;
+};
+
+enum class Direction {
+	North,
+	East,
+	South,
+	West
+};
