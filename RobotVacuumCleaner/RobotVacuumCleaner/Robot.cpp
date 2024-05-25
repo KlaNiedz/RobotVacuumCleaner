7138bfcf19ac6b3cbb1475b70807c94b6cdae5db
kniedzia <01186982@pw.edu.pl>
2024-05-25 21:23:18 +0200

    fixed move_right

--- RobotVacuumCleaner/RobotVacuumCleaner/Robot.cpp
+++ RobotVacuumCleaner/RobotVacuumCleaner/Robot.cpp
@@ -83,7 +83,7 @@ void Robot::go_left() {
 }
 
 void Robot::go_right() {
-	set_heading(Direction::West);
+	set_heading(Direction::East);
 	update_sensors();
 	if (not front_sensor.wall_detected() && front_sensor.get_dist_to_stop() > 0) {
 		move_right();
