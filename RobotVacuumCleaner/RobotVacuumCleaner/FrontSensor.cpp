69304a90074c95abe25c8aa884cb8fa3198f0618
Piotr Kozłowski <01186976@pw.edu.pl>
2024-05-26 21:03:06 +0200

    Corrected bug in FrontSensor. Now Benek won't pass the eastern wall.

--- RobotVacuumCleaner/RobotVacuumCleaner/FrontSensor.cpp
+++ RobotVacuumCleaner/RobotVacuumCleaner/FrontSensor.cpp
@@ -129,13 +129,13 @@ bool FrontSensor::wall_detected() const {
 		return false;
 		break;
 	case Direction::East:
-		if (get_x() == get_map().getWidth()) {
+		if (get_x() == get_map().getWidth() - 1) {
 			return true;
 		}
 		return false;
 		break;
 	case Direction::South:
-		if (get_y() == get_map().getHeight()) {
+		if (get_y() == get_map().getHeight() - 1) {
 			return true;
 		}
 		return false;
